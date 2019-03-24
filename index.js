3cf47e19a7cd8ca8a9475844687099911ae623b5
AlexandrKoliukh <alexkoliukh@gmail.com>
2019-03-24 12:24:25 +0300

    init VCS

---
+++ index.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+
+const fileContent = fs.readFileSync("sourceData.txt", "utf8");
+const sourceData = fileContent
+    .split('\n')
+    .filter(i => i !== '')
+    .map(i => i
+        .split(' ')
+        .filter(i => i !== ''));
+
+
+const voters = sourceData[0].slice().sort((a, b) => a - b);
+
+const superiorityCount = (arg1, arg2) => {
+    let temp = 0;
+    sourceData.map(i => {
+        if (i.indexOf(arg1) < i.indexOf(arg2)) temp += 1;
+    });
+    return temp;
+};
+
+const createAdjacencyMatrix = (superiorityCount) => {
+    const adjacencyMatrix =
+        Array.from(new Array(voters.length), () => new Array(voters.length).fill(0));
+
+    for (let i = 0; i < voters.length; i += 1) {
+        for (let j = 0; j < voters.length; j += 1) {
+            adjacencyMatrix[i][j] = superiorityCount(voters[i], voters[j]) > voters.length / 2 ? 1 : 0;
+        }
+    }
+    return adjacencyMatrix;
+};
+
+const methodCoupland = (adjacencyMatrix) => {
+    let result = new Array(voters.length),
+        temp = 0;
+
+    for (let i = 0; i < voters.length; i += 1) {
+        for (let j = 0; j < voters.length; j += 1) {
+            if (adjacencyMatrix[i][j] === 0) temp -= 1;
+            else temp += 1;
+        }
+        result[i] = temp + 1;
+        temp = 0;
+    }
+    return result;
+};
+
+console.log('\n Избиратели: \n');
+console.log(voters.map(i => `V${i}`));
+
+console.log('\n Исходные данные: \n');
+console.log(sourceData);
+
+console.log('\n Матрица смежности: \n');
+console.log(createAdjacencyMatrix(superiorityCount));
+
+console.log('\n Метод Коупленда: \n');
+console.log(methodCoupland(createAdjacencyMatrix(superiorityCount)));
+console.log('\n');
+
